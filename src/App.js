e5c3e291b1cd0546c0fed0e81d514729d694b985
Rafael Barreto <rafaelhbarreto@gmail.com>
2019-09-29 17:59:08 -0300

    adds history navigation

--- src/App.js
+++ src/App.js
@@ -2,21 +2,24 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import './config/ReactotronConfig';
+import { ToastContainer } from 'react-toastify';
 
 import GlobalStyles from './styles/global';
 import Routes from './routes';
 import Header from './components/Header';
+import history from './services/history';
 import store from './store';
 
 
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
+      <Router history={history}>
         <Header />
         <Routes />
         <GlobalStyles />
-      </BrowserRouter>
+        <ToastContainer autoClose={3000} />
+      </Router>
     </Provider>
   );
 }
